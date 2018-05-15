9c8e942c789fb582023018e3f98c906e79d2a6ee
zhzebiao <zhzebiao@163.com>
2018-05-15 10:23:01 +0800

    使用dataFrame的写法实现了WordCount 学习到的有： 1、使用自定义结构完成javaRdd到Dataframe的转化 2、使用函数式编程思想完成spark的操作 不足之处有： 1、不清楚如何使用一些聚合函数，如sort如何对前面聚合的结果进行排序

--- src/main/java/com/zzb/WordCount.java
+++ src/main/java/com/zzb/WordCount.java
@@ -3,17 +3,21 @@
  */
 package com.zzb;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoder;
-import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
 /**
  * @author ZP-209
@@ -21,11 +25,31 @@ import org.apache.spark.sql.SparkSession;
  */
 public class WordCount {
 	public static void main(String[] args) {
-		String logFile = "README.md";
-		SparkConf conf = new SparkConf().setMaster("local").setAppName("WordCount");
-		JavaSparkContext jsc = new JavaSparkContext(conf);
-		SQLContext sqlContext = new SQLContext(jsc);
-		JavaRDD<String> lines = jsc.textFile(logFile);
-		lines.map(o->Row(o))
+		String logFile = "word.txt";
+//		String logFile = "README.md";
+		//sparkSession会比原先的JavaContext包含的信息更多；
+		SparkSession spark =SparkSession
+				.builder()
+				.appName("WordCount")
+				.master("local")
+				.getOrCreate();
+		//获取text中的javaRdd
+		JavaRDD<String> lineRdd = spark.sparkContext().textFile(logFile, 1).toJavaRDD();
+		//自定义structType
+		List<StructField> fields = new ArrayList<StructField>();
+		StructField field = DataTypes.createStructField("word", DataTypes.StringType, true);
+		fields.add(field);
+		StructType schema = DataTypes.createStructType(fields);
+		//flatmap进行拆分扁平化，map进行类型转换
+		JavaRDD<Row> wordRdd = lineRdd.flatMap((FlatMapFunction<String, String>)record-> {
+			return Arrays.asList(record.split(" ")).iterator();
+		}).map((Function<String, Row>)record->{
+			return RowFactory.create(record);
+		});
+		//根据schema进行javaRdd到DataSet的转化
+		Dataset<Row> wordDataFrame = spark.createDataFrame(wordRdd, schema);
+		//进行分组累加
+		Dataset<Row> result = wordDataFrame.groupBy("word").count();
+		result.show();
 	}
 }
